1b029f64913cb2a80ec08c677307b84afb2a1532
Palomars22 <palomapoa2022@gmail.com>
2022-03-31 20:09:25 -0300

    created project

---
+++ src/app/Program.java
@@ -0,0 +1,77 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import entities.Sale;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		List<Sale> list = new ArrayList<>();
+
+		
+//		System.out.print("Enter file full path: ");
+//		String path = sc.nextLine();
+//		
+		System.out.println("Digite o arquivo path :");
+		String path=sc.nextLine();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			
+			
+		
+			String line = br.readLine();
+			while (line != null) {
+
+				String[] fields = line.split(",");
+				
+				Integer month=Integer.parseInt(fields[0]);
+				Integer year=Integer.parseInt(fields[1]);
+				String seller=fields[2];
+				Integer items=Integer.parseInt(fields[3]);
+				Double total=Double.parseDouble(fields[4]);
+			Sale s1=new Sale(month, year, seller, items, total)	;	
+			list.add(s1);
+			
+	
+	
+			line = br.readLine();
+			}
+			
+			Map<String, Double> counting = list.stream().collect(
+	                Collectors.groupingBy(Sale::getSeller, Collectors.summingDouble(value ->value.getTotal() )));
+			
+			System.out.println("--------------------------------------------");
+			System.out.println("Total de vendas por vendedor:");
+			System.out.println("--------------------------------------------");
+					counting.forEach((seller,total)->System.out.println(String.format(seller+" R$: %.2f",total)));
+System.out.println("_____________________________________________");	        
+		    
+		} catch (IOException e) {
+			System.out.println("Error : " + e.getMessage());
+		} finally {
+			if (sc != null) {
+
+				sc.close();
+			}
+
+		}
+	}
+
+}
---
+++ src/entities/Sale.java
@@ -0,0 +1,104 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class Sale {
+
+	private Integer month, year, items;
+	private String seller;
+	private Double total;
+	List<Double> list = new ArrayList<>();
+
+	public Sale(Integer month, Integer year, String seller, Integer items, Double total) {
+		super();
+		this.month = month;
+		this.year = year;
+		this.seller = seller;
+		this.items = items;
+		this.total = total;
+	}
+
+	public Sale() {
+		super();
+	}
+
+	public Integer getMonth() {
+		return month;
+	}
+
+	public void setMonth(Integer month) {
+		this.month = month;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public Integer getItems() {
+		return items;
+	}
+
+	public void setItems(Integer items) {
+		this.items = items;
+	}
+
+	public String getSeller() {
+		return seller;
+	}
+
+	public void setSeller(String seller) {
+		this.seller = seller;
+	}
+
+	public Double getTotal() {
+		list.add(total);
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	public Double AveragePrice() {
+		
+		double sum=0;
+		if(seller=="logan") {
+			sum+=total;
+				
+		}
+		
+		return sum;
+		
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(total);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(total, other.total);
+	}
+
+	@Override
+	public String toString() {
+		return "Sale [month=" + month + ", year=" + year + ", items=" + items + ", seller=" + seller + ", total="
+				+ total + "]";
+	}
+
+}
